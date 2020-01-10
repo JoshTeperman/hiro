826950a99048ae9150d7b82fc5cf368b37a51aac
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-10 23:12:55 +1100

    refactor weapon test

--- lib/hiro/items/weapons/weapon.rb
+++ lib/hiro/items/weapons/weapon.rb
@@ -1,10 +1,12 @@
 module Hiro
   module Items
     class Weapon
-      attr_reader :type, :equipped
-      def initialize(weapon_hash)
-        @type = weapon_hash[:type]
-        @equipped = weapon_hash[:equipped]
+      attr_reader :weapon_class, :name, :min_level, :base_damage
+      def initialize(weapon_class:, name:, min_level:, base_damage:)
+        @weapon_class = weapon_class
+        @name = name
+        @min_level = min_level
+        @base_damage = base_damage
       end
     end
   end
--- spec/hiro/items/weapons/weapon_spec.rb
+++ spec/hiro/items/weapons/weapon_spec.rb
@@ -2,21 +2,57 @@ module Hiro
   module Items
     module Weapons
       RSpec.describe Weapon do
-        subject { described_class.new(type: type, name: name, attack: attack, equipped: equipped) }
+        subject { described_class.new(params) }
 
-        describe 'sword' do
-          let(:type) { Weapons::Sword }
-          let(:name) { 'Mountain Smasher' }
-          let(:attack) { 3 }
-          let(:equipped) { true }
+        let(:params) do
+          {
+            weapon_class: weapon_class,
+            name: name,
+            min_level: min_level,
+            base_damage: base_damage,
+          }
+        end
 
-          it 'it has a type' do
-            expect(subject.type).to eq type
+        let(:name) { 'Weapon' }
+        let(:min_level) { 1 }
+        let(:base_damage) { 2 }
+        let(:weapon_class) { Weapons::Sword }
+
+        describe '#initialize' do
+          it 'has a weapon_class' do
+            expect(subject.weapon_class).to eq weapon_class
           end
 
-          it 'is equipped' do
-            expect(subject.equipped).to eq true
+          it 'has a name' do
+            expect(subject.name).to eq name
           end
+
+          it 'has a min_level' do
+            expect(subject.min_level).to eq min_level
+          end
+
+          it 'has base damage' do
+            expect(subject.base_damage).to eq base_damage
+          end
+
+          it 'has a default range of 1' do
+          end
+        end
+
+        describe '#set_base_damage' do
+        end
+
+        describe '#calculate_modified_damage' do
+          # TODO: Calculate on top of base damage
+        end
+
+        describe '#equip' do
+        end
+
+        describe '#unequip' do
+        end
+
+        describe '#drop' do
         end
       end
     end
