cd813495e988857fa37acc042e6795e9e4087757
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-12 21:24:09 +1100

    add weapon_class to weapon

--- lib/hiro/items/weapon.rb
+++ lib/hiro/items/weapon.rb
@@ -1,11 +1,19 @@
 module Hiro
   module Items
     class Weapon
-      attr_reader :name, :min_level, :base_damage, :range
+      attr_reader :weapon_class, :name, :min_character_level, :base_damage, :range
 
-      def initialize(name:, min_level:, base_damage:, range:)
+      def initialize(
+          weapon_class:,
+          name:,
+          min_character_level:,
+          base_damage:,
+          range:
+        )
+
+        @weapon_class = weapon_class
         @name = name
-        @min_level = min_level
+        @min_character_level = min_character_level
         @base_damage = base_damage
         @range = range
       end
--- spec/hiro/items/weapon_spec.rb
+++ spec/hiro/items/weapon_spec.rb
@@ -1,42 +1,42 @@
 module Hiro
   module Items
     RSpec.describe Weapon do
-      subject { described_class.new(params) }
-
-      let(:params) do
-        {
+      subject do
+        described_class.new(
+          weapon_class: weapon_class,
           name: name,
-          min_level: min_level,
+          min_character_level: min_character_level,
           base_damage: base_damage,
-          range: range
-        }
+          range: range,
+        )
       end
 
+      let(:weapon_class) { instance_double(Sword, type: 'Long Sword', min_damage: 2, max_damage: 10) }
       let(:name) { 'Godfather' }
-      let(:min_level) { 25 }
+      let(:min_character_level) { 25 }
       let(:base_damage) { 85 }
       let(:range) { 2 }
 
-
       describe '#initialize' do
-        it 'has a name' do
-          expect(subject.name).to eq name
-        end
-
-        it 'has a min_level' do
-          expect(subject.min_level).to eq min_level
-        end
-
-        it 'has base damage' do
-          expect(subject.base_damage).to eq base_damage
-        end
-
-        it 'has a range' do
-          expect(subject.range).to eq range
+        it 'has expected attributes' do
+          aggregate_failures do
+            expect(subject.weapon_class).to eq weapon_class
+            expect(subject.name).to eq name
+            expect(subject.min_character_level).to eq min_character_level
+            expect(subject.base_damage).to eq base_damage
+            expect(subject.range).to eq range
+          end
         end
       end
 
-      describe '#set_base_damage' do
+      describe '#roll_base_damage' do
+        let(:min_damage) { 2 }
+        let(:max_damage) { 10 }
+        let(:weapon_class) { OpenStruct.new(min_damage: min_damage, max_damage: max_damage) }
+
+        it 'sets a random base damage' do
+          expect(subject.roll_base_damage).to be_between(min_damage, max_damage)
+        end
       end
 
       describe '#calculate_modified_damage' do
