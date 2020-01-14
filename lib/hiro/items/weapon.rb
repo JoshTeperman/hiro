abc9d744a4f78ca3278125a398af9d570b57b8aa
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-15 08:01:22 +1100

    initialize Weapon Class with Struct

--- lib/hiro/items/weapon.rb
+++ lib/hiro/items/weapon.rb
@@ -4,21 +4,26 @@ module Hiro
       attr_reader :weapon_class, :name, :min_character_level, :base_damage, :range
 
       def initialize(
-        weapon_class:,
+        weapon_class_attributes:,
         name:,
         min_character_level:,
         range:
       )
-        @weapon_class = OpenStruct.new(weapon_class)
+        @weapon_class = Struct::WeaponClass.new(weapon_class_attributes)
         @name = name
         @min_character_level = min_character_level
-        @base_damage = roll_base_damage
+        @base_damage = weapon_class.roll_base_damage
         @range = range
       end
 
-      def roll_base_damage
-        rand(weapon_class.min_damage..weapon_class.max_base_damage)
+      private
+
+      Struct.new('WeaponClass', :min_damage, :max_damage, :max_base_damage, :type, keyword_init: true) do
+        def roll_base_damage
+          rand(min_damage..max_base_damage)
+        end
       end
+
     end
   end
 end
--- spec/hiro/items/weapon_spec.rb
+++ spec/hiro/items/weapon_spec.rb
@@ -3,61 +3,56 @@ module Hiro
     RSpec.describe Weapon do
       subject do
         described_class.new(
-          weapon_class: weapon_class,
+          weapon_class_attributes: weapon_class_attributes,
           name: name,
           min_character_level: min_character_level,
           range: range,
         )
       end
 
-      let(:weapon_class) { OpenStruct.new(type: type, min_damage: min_damage, max_damage: max_damage, max_base_damage: max_base_damage) }
-      let(:type) { 'Long Sword' }
-      let(:min_damage) { 2 }
-      let(:max_damage) { 10 }
-      let(:max_base_damage) { 4 }
-
-      let(:name) { 'Godfather' }
-      let(:min_character_level) { 25 }
-      let(:range) { 2 }
-      let(:base_damage) { 5 }
-
-      allow{ subject }.to receive(:weapon_class).and_return(weapon_class)
+      let(:weapon_class_attributes) { { type: type, min_damage: min_damage, max_damage: max_damage, max_base_damage: max_base_damage } }
 
       describe '#initialize' do
+        let(:type) { 'Long Sword' }
+        let(:min_damage) { 2 }
+        let(:max_damage) { 10 }
+        let(:max_base_damage) { 4 }
+
+        let(:name) { 'Godfather' }
+        let(:min_character_level) { 25 }
+        let(:range) { 2 }
+
         it 'has expected attributes' do
           aggregate_failures do
-            expect(subject.weapon_class).to eq weapon_class
             expect(subject.name).to eq name
             expect(subject.min_character_level).to eq min_character_level
             expect(subject.range).to eq range
             expect(subject.base_damage).to be_between(min_damage, max_base_damage)
           end
         end
-      end
 
-      describe 'weapon_class' do
-        it 'returns a Struct with the expected attributes' do
-          require 'pry';binding.pry
-          aggregate_failures do
-            expect(weapon_class.type).to eq type
-            expect(weapon_class.min_damage).to eq min_damage
-            expect(weapon_class.max_damage).to eq max_damage
-            expect(weapon_class.max_base_damage).to eq max_base_damage
+        context 'when it has no name' do
+          let(:name) { nil }
+
+          it 'it is a weapon without a name' do
+            expect(subject.name).to be_nil
           end
         end
-      end
 
-      describe '#calculate_modified_damage' do
-        # TODO: Calculate on top of base damage
-      end
+        describe 'weapon_class' do
+          let(:weapon_class) { subject.weapon_class }
 
-      describe '#equip' do
-      end
+          it 'creates an instance of Struct::WeaponClass with the expected attributes' do
+            aggregate_failures do
+              expect(weapon_class).to be_instance_of(Struct::WeaponClass)
+              expect(weapon_class.type).to eq type
+              expect(weapon_class.min_damage).to eq min_damage
+              expect(weapon_class.max_damage).to eq max_damage
+              expect(weapon_class.max_base_damage).to eq max_base_damage
 
-      describe '#unequip' do
-      end
-
-      describe '#drop' do
+            end
+          end
+        end
       end
     end
   end
