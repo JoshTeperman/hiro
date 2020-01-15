fd860ebceb92fe67c950677ddb5aff1324ea7003
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-15 21:56:08 +1100

    equipping items test stubs

--- lib/hiro/characters/player.rb
+++ lib/hiro/characters/player.rb
@@ -2,10 +2,12 @@ module Hiro
   module Characters
     class Player
       attr_reader :name, :attributes
+      attr_accessor :equipped_weapon
 
       def initialize(player_hash)
         @name = player_hash[:name]
         @attributes = player_hash[:attributes]
+        @equipped_weapon = nil
       end
     end
   end
--- spec/hiro/characters/player_spec.rb
+++ spec/hiro/characters/player_spec.rb
@@ -36,6 +36,65 @@ module Hiro
           expect(subject.attributes).to eq(attributes)
         end
       end
+
+      describe '#equipped_weapon' do
+        context 'when the player has a weapon equipped' do
+          let(:equipped_weapon) { instance_double(Items::Sword) }
+
+          before { subject.equipped_weapon = equipped_weapon }
+
+          it 'returns the equipped weapon' do
+            expect(subject.equipped_weapon).to eq(equipped_weapon)
+          end
+        end
+
+        context 'when the player has no weapon equipped' do
+          let(:equipped_weapon) { nil }
+
+          it 'returns nil' do
+            expect(subject.equipped_weapon).to be_nil
+          end
+        end
+      end
+
+      describe '#equip' do
+        let(:weapon) { instance_double(Items::Sword) }
+
+        # TODO create classes:
+        # let(:chest) { instance_double(Items::Chest) }
+        # let(:head) { instance_double(Items::Head) }
+        # let(:gloves) { instance_double(Items::Gloves) }
+        # let(:boots) { instance_double(Items::Boots) }
+        # let(:belt) { instance_double(Items::Belt)}
+        # let(:ring) { instance_double(Items::Ring) }
+        # let(:necklace) { instance_double(Items::Necklace) }
+        # let(:orb) { instance_double(Items::Orb) }
+
+        context 'when there are no equipped items' do
+          let(:equipped_gear) { {} }
+          let(:params) { { weapon: weapon } }
+
+          context 'and all of the items level requirements are equal to or below the character level' do
+            it 'equips all items' do
+            end
+          end
+
+          context 'and some of the items level requirements are equal to or below the character level' do
+            it 'equips all equippable items' do
+            end
+          end
+
+          context 'and none of the items level requirements are equal to or below the character level' do
+            it 'equips none of the items' do
+            end
+          end
+
+          context 'when equipping an item to an equipped slot' do
+            it 'updates equipped items' do
+            end
+          end
+        end
+      end
     end
   end
 end
