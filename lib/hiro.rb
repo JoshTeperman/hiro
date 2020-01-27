4fcc1f1d42cdee158a6bc7de1d5c1965c90d8038
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-28 09:10:26 +1100

    wip creating app entrypoint

--- lib/hiro.rb
+++ lib/hiro.rb
@@ -1,3 +1,7 @@
+lib = (File.expand_path('../lib', __dir__))
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'pry';binding.pry
 require 'hiro/version'
 require 'hiro/game/engine'
 require 'hiro/game/errors'
@@ -6,3 +10,10 @@ require 'hiro/items/weapon'
 require 'hiro/items/sword'
 require 'hiro/items/armour'
 require 'hiro/items/chest'
+
+begin
+  p 'Starting Hiro ...'
+  Engine.new
+rescue => e
+  p "Oops, something went wrong: #{e}"
+end
