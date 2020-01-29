2dbd0b481bf75f351a4db555d45daa75843cb9ed
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-30 09:03:39 +1100

    planning what Window / Map will look like

--- lib/hiro/game/window.rb
+++ lib/hiro/game/window.rb
@@ -4,7 +4,11 @@ module Hiro
   module Game
     class Window
       include Game::Errors
+
+      attr_reader :map, :entities
       def initialize
+        @map = map
+        @entities = []
         super(self)
       end
     end
--- spec/hiro/game/window_spec.rb
+++ spec/hiro/game/window_spec.rb
@@ -3,11 +3,29 @@ module Hiro
     RSpec.describe Window do
       it_behaves_like 'Errors'
 
-      subject { described_class.new() }
+      subject { described_class.new(map: map) }
 
       describe '#initialize' do
+        let(:map) do
+          {
+            [[" ", " "], [" ", " "]]
+
+          }
+        end
+        let(:entry_points) { [[0, 0]] }
+        let(:exit_points) { [[0, 0]] }
+
         it 'initializes without error' do
           expect { subject }.not_to raise_error
+        end
+
+        it 'has expected attributes' do
+          aggregate_failures do
+            expect(subject.map).to eq map
+            expect(subject.entities).to eq entities
+            expect(subject.entry_points).to eq entry_points
+            expect(subject.exit_points).to eq exit_points
+          end
         end
       end
     end
