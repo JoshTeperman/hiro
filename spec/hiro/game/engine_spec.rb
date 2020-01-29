b43024934b74576a88a2d602dcbee34ac6de4b6f
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-29 22:04:18 +1100

    Engine options loads saved player

--- spec/hiro/game/engine_spec.rb
+++ spec/hiro/game/engine_spec.rb
@@ -19,6 +19,32 @@ module Hiro
         end
       end
 
+      describe 'options' do
+        context 'when saved_player is given' do
+          let(:options) { { saved_player: saved_player } }
+          let(:saved_player) do
+            {
+              name: 'Saved Player',
+              life: 20,
+              mana: 50,
+              character_level: 4,
+              location: Game::Locations::HOME,
+              attributes: {
+                max_life: 55,
+                max_mana: 56,
+                strength: 13,
+                dexterity: 99,
+                vitality: 86,
+                intelligence: 11,
+              },
+            }
+          end
+
+          it 'loads the saved player' do
+            expect(subject.player.name).to eq saved_player[:name]
+          end
+        end
+      end
 
       describe '#input' do
       end
