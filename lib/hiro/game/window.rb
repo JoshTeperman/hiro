3ebc5e24bfbfd21b92f5edeb2fd77472bdd29ab3
Josh Teperman <Josh.teperman@weployapp.com>
2020-01-31 16:26:48 +1100

    Window.add_entity success scenario

--- lib/hiro/game/window.rb
+++ lib/hiro/game/window.rb
@@ -1,15 +1,27 @@
+require 'dry-monads'
 require 'hiro/game/errors_spec'
 
 module Hiro
   module Game
     class Window
+      include Dry::Monads[:result]
       include Game::Errors
 
       attr_reader :map, :entities
+
       def initialize(map:)
         @map = map
         @entities = []
         super(self)
+      end
+
+      def add_entity(entity)
+        if entities << entity
+          Dry::Monads::Success(entities)
+        else
+          add_error('Failed to add entity', :entities)
+          Dry::Monads::Failure(self)
+        end
       end
     end
   end
--- spec/hiro/game/window_spec.rb
+++ spec/hiro/game/window_spec.rb
@@ -3,16 +3,16 @@ require 'ostruct'
 module Hiro
   module Game
     RSpec.describe Window do
+      it_behaves_like 'Errors'
+
       subject { described_class.new(map: map) }
 
+      let(:map) { OpenStruct.new(shape: shape, entry_points: entry_points, exit_points: exit_points) }
+      let(:entry_points) { [[0, 0]] }
+      let(:exit_points) { [[0, 0]] }
+      let(:shape) { [[' ', ' '], [' ', ' ']] }
+
       describe '#initialize' do
-        it_behaves_like 'Errors'
-
-        let(:map) { OpenStruct.new(shape: shape, entry_points: entry_points, exit_points: exit_points) }
-        let(:entry_points) { [[0, 0]] }
-        let(:exit_points) { [[0, 0]] }
-        let(:shape) { [[' ', ' '], [' ', ' ']] }
-
         it 'initializes without error' do
           expect { subject }.not_to raise_error
         end
@@ -24,6 +24,18 @@ module Hiro
             expect(subject.map.entry_points).to eq entry_points
             expect(subject.map.exit_points).to eq exit_points
           end
+        end
+      end
+
+      describe '#add_entity' do
+        let(:entity) { instance_double(Characters::Player) }
+
+        it 'adds an entity to the entities array' do
+          expect { subject.add_entity(entity) }.to change(subject.entities, :length).by(1)
+        end
+
+        it 'returns the entity array' do
+          expect(subject.add_entity(entity).value!).to eq([entity])
         end
       end
     end
