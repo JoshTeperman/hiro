53266c31aa64e0820c157f5201038de164f0a4fa
Josh Teperman <Josh.teperman@weployapp.com>
2020-02-05 08:51:45 +1100

    stub map when Engine loads to OpenStruct for now

--- lib/hiro/game/engine.rb
+++ lib/hiro/game/engine.rb
@@ -4,9 +4,10 @@ module Hiro
       attr_reader :window, :player
 
       def initialize(options)
-        @window = Game::Window.new
+        @window = Game::Window.new(map: OpenStruct.new(shape: [[' ', ' '], [' ', ' ']], entry_points: [[0, 0]], exit_points: [[0, 0]]))
         @player = Characters::Player.new(options[:saved_player])
       end
     end
   end
 end
+
