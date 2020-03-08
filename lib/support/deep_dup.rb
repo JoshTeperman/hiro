8fbcd3aac7785880cdfbedccedd6c57f866498cb
Josh Teperman <Josh.teperman@weployapp.com>
2020-03-08 17:08:14 +1100

    recursion safety for deep_dup

--- lib/support/deep_dup.rb
+++ lib/support/deep_dup.rb
@@ -6,14 +6,14 @@ end
 
 class Array
   def deep_dup
-    self.map(&:deep_dup)
+    map { |element| element == self ? self : element.deep_dup }
   end
 end
 
 class Hash
   def deep_dup
-    hash = Hash.new
-    self.each_pair { |k, v| hash[k] = v.deep_dup }
+    hash = {}
+    each_pair { |key, value| hash[key] = value == self ? self : value.deep_dup }
 
     hash
   end
