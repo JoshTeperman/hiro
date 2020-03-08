57fcbf754909bb8da59db9e58ac8140c6aafcc99
Josh Teperman <Josh.teperman@weployapp.com>
2020-03-08 16:41:53 +1100

    fix move directions

--- lib/hiro/characters/player.rb
+++ lib/hiro/characters/player.rb
@@ -40,11 +40,11 @@ module Hiro
       end
 
       def move_up
-        @y += 1
+        @y -= 1
       end
 
       def move_down
-        @y -= 1
+        @y += 1
       end
 
       def move_right
