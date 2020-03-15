ffa2a4be0a2473a5edbe118adfd0f8472c792bc1
Josh Teperman <Josh.teperman@weployapp.com>
2020-03-15 18:24:39 +1100

    roll_damage methods should be class methods

--- lib/hiro/items/weapon.rb
+++ lib/hiro/items/weapon.rb
@@ -9,11 +9,19 @@ module Hiro
         @weapon_class = Struct::WeaponClass.new(weapon_class_attributes)
         @name = name
         @min_character_level = min_character_level
-        @min_damage = weapon_class.roll_min_damage
-        @max_damage = weapon_class.roll_max_damage
+        @min_damage = roll_min_damage
+        @max_damage = roll_max_damage
         @range = range
 
         super(self)
+      end
+
+      def roll_min_damage
+        rand(@weapon_class.min_minimum_damage..@weapon_class.max_minimum_damage)
+      end
+
+      def roll_max_damage
+        rand(@weapon_class.min_maximum_damage..@weapon_class.max_maximum_damage)
       end
 
       Struct.new(
@@ -24,15 +32,7 @@ module Hiro
         :min_maximum_damage,
         :max_maximum_damage,
         keyword_init: true
-      ) do
-        def roll_min_damage
-          rand(min_minimum_damage..max_minimum_damage)
-        end
-
-        def roll_max_damage
-          rand(min_maximum_damage..max_maximum_damage)
-        end
-      end
+      )
     end
   end
 end
